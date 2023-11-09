60d0947e3e779bd039ac6b9fc10467f87b90c72a
Kondorova Nastya <kondorovanastya@gmail.com>
2023-11-09 20:06:06 +0400

    SteppingDifferentialOperator, LeftSteppingDifferentialOperator, RightSteppingDifferentialOperator, MiddleSteppingDifferentialOperator, DifferentialOperator

--- src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/operations/MiddleSteppingDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/operations/MiddleSteppingDifferentialOperator.java
@@ -5,8 +5,6 @@ public class MiddleSteppingDifferentialOperator extends SteppingDifferentialOper
     public MiddleSteppingDifferentialOperator(double step) {
         super(step);
     }
-
-    @Override
     public MathFunction derive(MathFunction function) {
         return (x) -> (function.apply(x + step) - function.apply(x - step)) / (2 * step);
     }

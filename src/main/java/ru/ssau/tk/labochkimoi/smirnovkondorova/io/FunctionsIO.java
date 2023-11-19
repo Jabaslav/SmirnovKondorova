baae919f58567377f8221b9bf574a140ae4e5be1
Jabaslav <revigamingplay@gmail.com>
2023-11-19 19:16:36 +0400

    5.5 deserialize

--- src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/io/FunctionsIO.java
@@ -83,5 +83,8 @@ final class FunctionsIO {
         objectOutputStream.flush();
     }
 
-
+    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(stream);
+        return ((TabulatedFunction) objectInputStream.readObject());
+    }
 }

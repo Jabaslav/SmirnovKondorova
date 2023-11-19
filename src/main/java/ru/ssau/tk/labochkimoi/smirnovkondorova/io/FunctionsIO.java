272779176cc2fe1ab05c78939e5fcb6baaa54672
Kondorova Nastya <kondorovanastya@gmail.com>
2023-11-19 16:25:37 +0400

    readTabulatedFunction

--- src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/labochkimoi/smirnovkondorova/io/FunctionsIO.java
@@ -5,6 +5,11 @@ import ru.ssau.tk.labochkimoi.smirnovkondorova.functions.factory.TabulatedFuncti
 import ru.ssau.tk.labochkimoi.smirnovkondorova.functions.Point;
 
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 final class FunctionsIO {
     private FunctionsIO() {
@@ -19,5 +24,30 @@ final class FunctionsIO {
             }
 
             printWriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        try {
+            int count = Integer.parseInt(reader.readLine());
+
+            double[] xValues = new double[count];
+            double[] yValues = new double[count];
+
+            NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+            for (int i = 0; i < count; i++) {
+                String line = reader.readLine();
+
+                String[] parts = line.split(" ");
+
+                xValues[i] = numberFormat.parse(parts[0]).doubleValue();
+                yValues[i] = numberFormat.parse(parts[1]).doubleValue();
+            }
+
+            return factory.create(xValues, yValues);
+
+        } catch (ParseException e) {
+            throw new IOException("Error parsing double values.", e);
+        }
     }
 }
